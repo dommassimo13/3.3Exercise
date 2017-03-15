f77cb5d7772a7788df42b6b3d7d75cf1357dfe15
dommassimo13 <dominic.s.massimo@gmail.com>
2017-03-15 09:56:58 -0400

    Added javadoc comments

--- src/IfExercise.java
+++ src/IfExercise.java
@@ -2,14 +2,21 @@ import javax.swing.*;
 
 /**
  * Created by Dominic Massimo on 3/15/2017.
+ * IfDemo
+ * Demonstrates if/else statement for username authentication
  */
 public class IfExercise {
     public static void main(String[] args) {
+        // declares string uname for dialog box
         String uname = JOptionPane.showInputDialog(null, "Enter your name ");
+        //declares correct username
         if (uname.equals("Dom")) {
         String pw = JOptionPane.showInputDialog(null, "Enter password ");
+        //declares correct password
         if (pw.equals("d123")) {
+            //If username is true then the correct response will be welcome + username
         JOptionPane.showMessageDialog(null, "Welcome " + uname);
+        // if username or password is false it will display either wrong password or wrong username
     } else {
         JOptionPane.showMessageDialog(null, "Wrong Password");
     }
@@ -17,6 +24,6 @@ public class IfExercise {
             JOptionPane.showMessageDialog(null,"Wrong Username");
         }
 
-            
+
     }
 }
